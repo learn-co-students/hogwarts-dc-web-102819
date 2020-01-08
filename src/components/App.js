826bfed5e722c8921c6b792986502b139be0ba0c
Kyle <kylepcole@gmail.com>
2020-01-08 15:13:44 -0500

    hogs are rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
+import Controls from './Controls'
+import HogsContainer from './HogsContainer'
 import hogs from '../porkers_data';
 
 class App extends Component {
+	constructor(){
+		super()
+		this.state={
+			hogs: hogs
+		}
+	}
+
+	filterHogs = (event) => {
+		console.log('filterHogs')
+	}
+
+	sortHogs = (event) => {
+		console.log('sortHogs')
+	}
+
   render() {
     return (
       <div className="App">
-          < Nav />
+          <Nav />
+          <Controls />
+          <HogsContainer hogs={hogs}/>
 
       </div>
     )
