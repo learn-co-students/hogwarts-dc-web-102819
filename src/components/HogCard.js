ad0cd6f4bec8e0b47dd272baf778d34de0f84b95
Kyle <kylepcole@gmail.com>
2020-01-08 17:55:46 -0500

    pigs hide, random image generates

--- src/components/HogCard.js
+++ src/components/HogCard.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import piggy from '../porco.png'
+// import piggy from '../porco.png'
 
 class HogCard extends React.Component {
 	constructor() {
 		super()
 		this.state = {
-			showDetails: false
+			showDetails: false,
+			className: 'ui card',
+			imgURL: null
 		}
 	}
 
@@ -16,39 +18,68 @@ class HogCard extends React.Component {
 	}
 
 	hideHog = () => {
-		
+		console.log('hiding')
+		this.setState({
+			className: 'no-display'
+		})
+	}
+
+	fetchHogImage(){
+		console.log('fetching')
+		var image = ''
+		fetch('https://source.unsplash.com/random/200x200/?pig')
+			.then(response => response.url)
+		// console.log(image)
+
+		// return image
+	}
+
+	componentDidMount = () => {
+		fetch('https://source.unsplash.com/random/200x200/?pig')
+			.then(response => {
+				this.setState({
+					imgURL: response.url
+				}) 
+			})
 	}
 
 	render() {
+		let URL = 'https://source.unsplash.com/random/200x200/?pig'
 		const {name, specialty, greased, weight, 'highest medal achieved': medal} = this.props.hog
 		return(
-			<div className="ui card">
+			<div className={this.state.className}>
 				<div className="card">
 			    <div className="image">
-			      <img src={piggy} alt={name}/>
+			      <img src='https://source.unsplash.com/random/200x200/?pig' alt={name}/>
 			    </div>
 			    <div className="content">
 			      <div className="header">{name}</div>
 				  	  {this.state.showDetails ? 
 						<div>
-							<p>Specialty: {specialty}</p>
-							<p>Greased: {greased.toString()}</p>
-							<p>Weight: {weight}</p>
-							<p>Highest Medal Achieved: {medal}</p>
 							<button 
 								className="ui button secondary" 
 								onClick={this.toggleDetails}> 
 								Hide Details 
+							</button>
+							<button className="ui icon button" onClick={this.hideHog}>
+		  						<i className="low vision icon"></i>
 							</button> 
+							<p>Specialty: {specialty}</p>
+							<p>Greased: {greased.toString()}</p>
+							<p>Weight: {weight}</p>
+							<p>Highest Medal Achieved: {medal}</p>
 						</div>: 
-					  <button 
-					  	  className="ui button primary"
-				  		  onClick={this.toggleDetails}>
-			        	  Show Details
-			      	  </button>}
-					  <button className="ui icon button" onClick={this.hideHog}>
-  						<i className="low vision icon"></i>
-					  </button>
+						<div>
+						  	<button 
+						  	  className="ui button primary"
+					  		  onClick={this.toggleDetails}>
+				        	  Show Details
+				      	  	</button>
+					      	<button className="ui icon button" onClick={this.hideHog}>
+		  						<i className="low vision icon"></i>
+							</button>
+						</div>}
+					  
 				</div>
 			  </div>
 			</div>
